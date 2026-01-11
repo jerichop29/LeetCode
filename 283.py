8f12a92999ce5d39fb2a6dc5078cdb6ea8c231a9
jerichopecho <jerichopecho84@gmail.com>
2026-01-11 17:06:54 +0800

    solution for problem 283.

---
+++ 283.py
@@ -0,0 +1,19 @@
+class Solution:
+    def moveZeroes(nums):
+        left = 0
+        
+        for right in range(len(nums)):
+            # Check if the value of right pointer is zero
+            if nums[right] != 0:
+                #Swap the value of left and right pointer
+                nums[left], nums[right] = nums[right], nums[left]
+                #increase the left pointer position
+                left += 1
+            else:
+                pass
+        
+        return nums
+        
+print(Solution.moveZeroes([0,1,0,3,5,1]))
+                
+                
