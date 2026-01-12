7ecf480c33650039d4c5c8e74119f3a09524f3b9
jerichopecho <jerichopecho84@gmail.com>
2026-01-12 15:46:58 +0800

    display seen dictionary

--- ArrayAndHashing/3.TwoSum.py
+++ ArrayAndHashing/3.TwoSum.py
@@ -1,12 +1,13 @@
 class Solution:
     def twoSum(nums, target):
         seen = {}
-
+        
         for i, num in enumerate(nums):
             complementary = target - num
             if complementary not in seen:
                 seen[num] = i
             else:
+                print(seen)
                 return [seen[complementary], i]
 
 #print(Solution.twoSum([3,4,5,6], 7)) [0,1]
