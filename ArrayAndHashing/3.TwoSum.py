dc2815e211a9ef98fb80936570b310ce7d00cc6d
jerichopecho <jerichopecho84@gmail.com>
2026-01-12 15:45:13 +0800

    Two Sum Solution

---
+++ ArrayAndHashing/3.TwoSum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(nums, target):
+        seen = {}
+
+        for i, num in enumerate(nums):
+            complementary = target - num
+            if complementary not in seen:
+                seen[num] = i
+            else:
+                return [seen[complementary], i]
+
+#print(Solution.twoSum([3,4,5,6], 7)) [0,1]
