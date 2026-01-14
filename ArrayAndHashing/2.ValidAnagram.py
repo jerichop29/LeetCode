4a67e93b0e865c51e790c7eace4690ede7024a05
Jericho Pecho <jerichopecho84@gmail.com>
2026-01-14 18:26:08 +0800

    add comments for valid anagram solution

--- ArrayAndHashing/2.ValidAnagram.py
+++ ArrayAndHashing/2.ValidAnagram.py
@@ -1,7 +1,8 @@
-from collections import Counter
+from collections import Counter #counter modules that counts the number of every character as key:value dictionary
 
 class Solution:
     def isAnagram(s, t):
+        #get the count of every character and compare if its equal
         if Counter(s) != Counter(t):
             return False
         return True
